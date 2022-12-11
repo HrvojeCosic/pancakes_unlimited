b167d4012d0ea80ddd0eda0a09ea17e1bc21a13d
Hrvoje Cosic <62515219+HrvojeCosic@users.noreply.github.com>
2022-12-11 09:57:23 +0100

    fix: CORS configuration

--- demo/src/main/java/com/example/pancakes_unlimited/ApplicationSecurityConfiguration.java
+++ demo/src/main/java/com/example/pancakes_unlimited/ApplicationSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.pancakes_unlimited;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -49,13 +53,23 @@ public class ApplicationSecurityConfiguration {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize ->
+        http.cors().and().authorizeHttpRequests(authorize ->
                         authorize
                                 .requestMatchers("/api/v1/ingredient/*").hasAuthority("EMPLOYEE")
                                 .requestMatchers("/api/v1/order/*", "/api/v1/pancake/*").hasAuthority("CUSTOMER")
                                 .requestMatchers("/api/v1/report/*").hasAuthority("STORE_OWNER"))
-                .formLogin();
+                .httpBasic();
         return http.build();
     }
 }
